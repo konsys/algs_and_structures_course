0b702f0d519e249e671cb470619e480309133658
Mona Lisa <you@example.com>
2025-03-04 12:13:21 +0300

    fix

--- 4-bubble-sort.js
+++ 4-bubble-sort.js
@@ -1,17 +1,19 @@
 const arr = [0, 3, 2, 5, 6, 8, 23, 9, 4, 2, 1, 2, 9, 6, 4, 1, 7, -1, -5, 23, 6, 2, 35, 6, 3, 32, 9, 4, 2, 1, 2, 9, 6, 4, 1, 7, -1, -5, 23, 9, 4, 2, 1, 2, 9, 6, 4, 1, 7, -1, -5, 23,]
 let count = 0
 
-function bubbleSort(array) {
-
-    for (let i = 0; i < array.length; i++) {
-        const element1 = array[i];
-        for (let j = 0; j < array.length; j++) {
-            const element2 = array[j];
-
+function bubbleSort(ar) {
+    for (let i = 0; i < ar.length; i++) {
+        for (let j = 0; j < ar.length - 1; j++) {
+            if (ar[j] > ar[j + 1]) {
+                const tmp = ar[j];
+                ar[j] = ar[j + 1];
+                ar[j + 1] = tmp;
+            }
         }
-
     }
+    return ar
 }
+
 
 console.log('length', arr.length)
 console.log(bubbleSort(arr)) // O(n*n)
--- 4_bubble_sort.js
+++ 4_bubble_sort.js
@@ -1,4 +1,4 @@
-const arr = [0,3,2,5,6,8,23,9,4,2,1,2,9,6,4,1,7,-1, -5, 23,6,2,35,6,3,32,9,4,2,1,2,9,6,4,1,7,-1, -5, 23,9,4,2,1,2,9,6,4,1,7,-1, -5, 23,]
+const arr = [0, 3, 2, 5, 6, 8, 23, 9, 4, 2, 1, 2, 9, 6, 4, 1, 7, -1, -5, 23, 6, 2, 35, 6, 3, 32, 9, 4, 2, 1, 2, 9, 6, 4, 1, 7, -1, -5, 23, 9, 4, 2, 1, 2, 9, 6, 4, 1, 7, -1, -5, 23,]
 let count = 0
 
 function bubbleSort(array) {
@@ -6,10 +6,10 @@ function bubbleSort(array) {
         for (let j = 0; j < array.length; j++) {
             if (array[j + 1] < array[j]) {
                 let tmp = array[j]
-                array[j] = array[j+1]
-                array[j+1] = tmp
+                array[j] = array[j + 1]
+                array[j + 1] = tmp
             }
-            count+=1
+            count += 1
         }
     }
     return array
